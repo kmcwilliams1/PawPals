dbbbf721a1d762302ac9d4497cf8bf0c70f06b25
TheJaguarKing <eddie.diaz@me.com>
2021-09-15 11:49:29 -0500

    drew relationships btw models

--- models/Comment.js
+++ models/Comment.js
@@ -5,10 +5,35 @@ class Comment extends Model {}
 
 Comment.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     body: {
       type: DataTypes.STRING,
       allowNull: false
-    }
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    status_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'status',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
