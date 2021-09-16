e592fab9936f52f7d4e5b571867bd9f0c1b276b3
Kyle McWilliams <kmac52694@gmail.com>
2021-09-16 07:50:12 -0500

    almost all working front end parts

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,73 @@
 let editEl = document.querySelector('.edit')
-let modalEl = document.querySelector('.modal')
-let modalSaveEl = document.querySelector('.modal-save')
+let bioModalEl = document.querySelector('.bioModal')
+let BioModalSaveEl = document.querySelector('.modal-save')
 let modalCancelEl = document.querySelector('.modal-cancel')
 let modalDeleteEl = document.querySelector('.delete')
 
-editEl.addEventListener('click', modalPop)
-function modalPop(){
-    modalEl.classList.add('is-active')
+
+// let signUpEl = document.querySelector('.signUp')
+// let signUpModalEl =  document.querySelector('.signUpModalEl')
+// let logInEl = document.querySelector('.logIn')
+// let logInModalEl =  document.querySelector('.logInModal')
+
+
+
+editEl.addEventListener('click', bioModalPop)
+function bioModalPop(){
+    bioModalEl.classList.add('is-active')
 };
 
-
 // to save bio edits, will need to .map this and json it
-modalSaveEl.addEventListener('click', modalDelete)
+BioModalSaveEl.addEventListener('click', modalDelete)
 function modalDelete(){
-    modalEl.classList.remove('is-active')
+    bioModalEl.classList.remove('is-active')
     // push to {{bio}}
     //add function to push to json
 };
 
-
 modalCancelEl.addEventListener('click', modalDelete)
 function modalDelete(){
-    modalEl.classList.remove('is-active')
+    bioModalEl.classList.remove('is-active')
 };
 
 modalDeleteEl.addEventListener('click', modalDelete)
 function modalDelete(){
-    modalEl.classList.remove('is-active')
+    bioModalEl.classList.remove('is-active')
 };
+
+
+
+
+// logInEl.addEventListener('click', logInModalPop)
+// function logInModalPop(){
+//     logInModalEl.classList.add('is-active')
+// };
+
+// modalCancelEl.addEventListener('click', modalDelete)
+// function modalDelete(){
+//     logInModalEl.classList.remove('is-active')
+// };
+
+// modalDeleteEl.addEventListener('click', modalDelete)
+// function modalDelete(){
+//     logInModalEl.classList.remove('is-active')
+// };
+
+
+
+
+// signUpEl.addEventListener('click', signUpModalPop)
+// function signUpModalPop(){
+//     signUpModalEl.classList.add('is-active')
+// };
+
+// modalCancelEl.addEventListener('click', modalDelete)
+// function modalDelete(){
+//     signUpModalEl.classList.remove('is-active')
+// };
+
+// modalDeleteEl.addEventListener('click', modalDelete)
+// function modalDelete(){
+//     signUpModalEl.classList.remove('is-active')
+// };
 
