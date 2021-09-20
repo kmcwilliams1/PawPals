69ba358f513d835156d2f05e73261eba7e3cca1d
Dylan <dylanfontenot@protonmail.com>
2021-09-19 19:17:18 -0500

    uncommented location replace

--- public/js/post.js
+++ public/js/post.js
@@ -14,7 +14,7 @@ const post = async (event) => {
     });
   
     if (response.ok) {
-      // document.location.replace('/');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
