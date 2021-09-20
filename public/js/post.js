2e7fc48e541878d382ea6f6d91f683fb365ebcfc
Dylan <dylanfontenot@protonmail.com>
2021-09-19 19:14:28 -0500

    fixed front end javascript by changing variables

--- public/js/post.js
+++ public/js/post.js
@@ -4,6 +4,7 @@
 const post = async (event) => {
     event.preventDefault();
     let description = document.querySelector("#text-box").value
+    console.log(description)
     const response = await fetch('/api/post', {
       method: 'POST',
       body:JSON.stringify ({
@@ -13,7 +14,7 @@ const post = async (event) => {
     });
   
     if (response.ok) {
-      document.location.replace('/');
+      // document.location.replace('/');
     } else {
       alert(response.statusText);
     }
--- public/js/signup.js
+++ public/js/signup.js
@@ -4,7 +4,7 @@ const signupFormHandler = async (event) => {
     const username = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-  
+  console.log(email,username,password)
     if (username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
